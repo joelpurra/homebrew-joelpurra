99b666907063b9175df512e3a26c0b0275407133
Joel Purra <code+github@joelpurra.com>
2015-01-23 08:08:59 +0100

    brew audit --strict shell-keystroke-animator.rb

--- shell-keystroke-animator.rb
+++ shell-keystroke-animator.rb
@@ -1,7 +1,7 @@
 class ShellKeystrokeAnimator < Formula
   homepage "https://github.com/joelpurra/shell-keystroke-animator"
-  url "https://github.com/joelpurra/shell-keystroke-animator/archive/v1.1.0.tar.gz"
   head "https://github.com/joelpurra/shell-keystroke-animator.git"
+  url "https://github.com/joelpurra/shell-keystroke-animator/archive/v1.1.0.tar.gz"
   sha256 "6efec10f007009e6f4b6dd8eb0ff0a2df763dd37d33085c0586dd66f3cb5b888"
 
   class Foo < Formula
@@ -10,13 +10,14 @@ class ShellKeystrokeAnimator < Formula
   end
 
   def install
-    prefix.install Dir["src"]
+    prefix.install "src"
     bin.install_symlink prefix/"src/keystroke-animator"
   end
 
   test do
     # These tests run Mac OS X automation tools to take screenshots, and
     # aren't expected to actually work on a build server.
+    # TODO: is there a `pipe_input`, because splitting these commands up with the echo pipe doesn't work.
     system "echo 'true' | #{bin}/keystroke-animator 0 0.01 0.01"
     assert (testpath/"output.gif").exist?,
             "output.gif was not created."
