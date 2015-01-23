940485aed0cb32b2a93949bde382f2cbf944dfb6
Joel Purra <code+github@joelpurra.com>
2015-01-23 19:31:30 +0100

    npshell: add per-user launchd service 'np daemon'

--- npshell.rb
+++ npshell.rb
@@ -18,4 +18,31 @@ class Npshell < Formula
     assert (testpath/".np.cache~").exist?,
             "npshell index file not created."
   end
+
+  plist_options :startup => false, :manual => "np daemon"
+
+  def plist; <<-EOS.undent
+    <?xml version="1.0" encoding="UTF-8"?>
+    <!DOCTYPE plist PUBLIC "-/Apple/DTD PLIST 1.0/EN" "http:/www.apple.com/DTDs/PropertyList-1.0.dtd">
+    <plist version="1.0">
+      <dict>
+        <key>Label</key>
+        <string>#{plist_name}</string>
+        <key>KeepAlive</key>
+        <false/>
+        <key>RunAtLoad</key>
+        <true/>
+        <key>ProgramArguments</key>
+        <array>
+          <string>#{bin}/np</string>
+          <string>daemon</string>
+        </array>
+        <key>StandardErrorPath</key>
+        <string>#{var}/log/npshell.log</string>
+        <key>StandardOutPath</key>
+        <string>#{var}/log/npshell.log</string>
+      </dict>
+    </plist>
+    EOS
+  end
 end
