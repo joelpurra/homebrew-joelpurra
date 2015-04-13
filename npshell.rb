68ddb3440565900c31061400d47ec50e2646ae0c
Joel Purra <code+github@joelpurra.com>
2015-04-13 22:19:06 +0200

    npshell v1.1.0

--- npshell.rb
+++ npshell.rb
@@ -1,11 +1,33 @@
 class Npshell < Formula
   homepage "https://github.com/joelpurra/npshell"
   head "https://github.com/joelpurra/npshell.git"
-  url "https://github.com/joelpurra/npshell.git", :tag => "v1.0.1"
+  url "https://github.com/joelpurra/npshell.git", :tag => "v1.1.0"
 
   depends_on "bash"
   depends_on "coreutils"
   depends_on "fswatch"
+  option "with-terminal-notifier", "Enables OS X notification support."
+  depends_on "terminal-notifier" => :optional
+
+  if build.with? "terminal-notifier"
+    def caveats; <<-EOS.undent
+      Notifications are not autostarted by brew; to do so
+      add the following line to your ~/.bash_profile:
+          np notify --is-running || ( np notify & )
+
+      To avoid getting error messages running `np daemon` without `launchd`,
+      add the following line to your ~/.bash_profile:
+          np daemon --is-running || ( np daemon & )
+      EOS
+    end
+  else
+    def caveats; <<-EOS.undent
+      The daemon can also run when you start your terminal.
+      Add the following line to your ~/.bash_profile:
+          np daemon --is-running || ( np daemon & )
+      EOS
+    end
+  end
 
   def install
     prefix.install "src"
