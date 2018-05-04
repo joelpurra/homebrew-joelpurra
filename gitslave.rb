6dc67583e66bc0ab2ed06858b490594e6a304913
Joel Purra <mig@joelpurra.se>
2018-05-04 12:22:34 +0200

    Audit gitslave

--- gitslave.rb
+++ gitslave.rb
@@ -1,11 +1,12 @@
 require 'formula'
 
 class Gitslave < Formula
+  desc "Unofficial fork of gitslave, hopefully more active than the original."
   homepage 'https://github.com/joelpurra/gitslave'
   head 'https://github.com/joelpurra/gitslave.git'
 
   devel do
-    version 'v2.0.2-joelpurra-51-g203467d'
+    version '2.0.2-joelpurra-51-g203467d'
     url 'https://github.com/joelpurra/gitslave/archive/v2.0.2-joelpurra-51-g203467d.tar.gz'
     sha256 '21ba3d4d8d9a70b4d5c09f89db3881dbc05b3db9de2b7a512f233a6aff80a464'
   end
