a4dffdd51238eae5d34ae3ac84f5db6a13ac28f3
Joel Purra <mig@joelpurra.se>
2018-05-04 14:18:31 +0200

    Remove tests from shell-keystroke-animator
    - Tests are run by homebrew in a sandbox.
    - The environment doesn't match the "real" user environment.
    - Due to security checks in Apple's osascript, tests fail due to a privilege violation when accessing user processes and windows.
    - The problem might be newer than macOS 10.6, but securiy changes were made then.
    - Probably more related to the way homebrew executes tests.
    
    See
    
    - https://developer.apple.com/library/content/releasenotes/AppleScript/RN-AppleScript/RN-10_6/RN-10_6.html

--- shell-keystroke-animator.rb
+++ shell-keystroke-animator.rb
@@ -11,19 +11,4 @@ class ShellKeystrokeAnimator < Formula
     prefix.install "src"
     bin.install_symlink prefix/"src/keystroke-animator"
   end
-
-  test do
-    # These tests run Mac OS X automation tools to take screenshots, and
-    # aren't expected to actually work on a build server.
-    require "open3"
-    Open3.popen3("#{bin}/keystroke-animator", "0", "0.01", "0.01", "0.01") do |stdin, stdout, _|
-      stdin.write("true
-")
-      stdin.close
-      # Wait for all the output.
-      stdout.read
-    end
-    assert (testpath/"output.gif").exist?,
-            "output.gif was not created."
-  end
 end
